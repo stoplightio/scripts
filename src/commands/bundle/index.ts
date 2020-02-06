4baa8c0a9885234930069d7da99420a5e5262fc8
Jakub Rożek <jakub@stoplight.io>
2020-02-06 15:24:38 +0700

    feat: bundle command

--- src/commands/bundle/index.ts
+++ src/commands/bundle/index.ts
@@ -1,112 +1,24 @@
-import { Command, flags as flagHelpers } from '@oclif/command';
-import cli from 'cli-ux';
-import * as fs from 'fs';
-import { dirname } from 'path';
-import { cp, mkdir } from 'shelljs';
+import { buildCommand, getConfigFilePath } from '../../utils';
+import BuildCommand from '../build';
 
-import { buildCommand, buildPath, getConfigFilePath, runCommand } from '../../utils';
-
-const _pick = require('lodash/pick');
-
-export default class BuildCommand extends Command {
-  public static strict = false;
-
+export default class BundleCommand extends BuildCommand {
   public static description = 'Bundle source code';
 
   public static examples = [`$ sl-scripts bundle`];
 
-  public static args = [];
+  protected get command() {
+    const parsed = this.parse(BundleCommand);
 
-  public static flags = {
-    verbose: flagHelpers.boolean({
-      description: 'moar logs',
-      required: false,
-    }),
-  };
-
-  public async run() {
-    cli.action.start('bundling...', undefined, { stdout: true });
-
-    const parsed = this.parse(BuildCommand);
-
-    const commands = [];
-
-    commands.push(`${buildCommand('rimraf')} dist`);
-
-    commands.push(
-      buildCommand(`rollup --config ${getConfigFilePath('rollup.config.js')}`, {
-        rawArgs: parsed.raw,
-        flags: Object.keys(BuildCommand.flags),
-      }),
-    );
-
-    if (parsed.flags.verbose) {
-      this.log(`commands:`);
-      for (const command of commands) {
-        this.log(`    '${command}'`);
-      }
-    }
-
-    for (const command of commands) {
-      runCommand(command);
-    }
-
-    cli.action.stop();
-
-    this.postPublish();
+    return buildCommand(`rollup --config ${getConfigFilePath('rollup.config.js')}`, {
+      rawArgs: parsed.raw,
+      flags: Object.keys(BundleCommand.flags),
+    });
   }
 
-  public postPublish() {
-    cli.action.start('copying extra files to dist folder...', undefined, {
-      stdout: true,
-    });
-
-    const pkg = JSON.parse(fs.readFileSync(buildPath('package.json')) as any);
-    const releasePkg = _pick(pkg, [
-      'name',
-      'version',
-      'description',
-      'keywords',
-      'typings',
-      'sideEffects',
-      'files',
-      'author',
-      'repository',
-      'license',
-      'engines',
-      'optionalDependencies',
-      'peerDependencies',
-      'dependencies',
-      'pkg',
-    ]);
-
-    releasePkg.main = 'index.cjs.js';
-    releasePkg.module = 'index.es.js';
-    if (!('typings' in releasePkg)) {
-      releasePkg.typings = 'index.d.ts';
-    }
-
-    // Convert any yalced dependencies (local "file:" dependencies) into proper bundledDependencies before publishing
-    for (const [name, version] of Object.entries(releasePkg.dependencies as { [key: string]: string })) {
-      if (version.startsWith('file:')) {
-        const filepath = version.replace('file:', '');
-        const installPath = buildPath('dist', 'node_modules', name);
-        mkdir('-p', dirname(installPath));
-        cp('-r', buildPath(filepath), installPath);
-        if (!releasePkg.bundledDependencies) {
-          releasePkg.bundledDependencies = [];
-        }
-        releasePkg.bundledDependencies.push(name);
-        // Remove the dependency from 'dependencies' since yarn (unlike npm) isn't smart enough
-        // to know bundledDependencies don't be installed from registry
-        delete releasePkg.dependencies[name];
-      }
-    }
-
-    fs.writeFileSync(buildPath('dist', 'package.json'), JSON.stringify(releasePkg, null, 2));
-    fs.copyFileSync(buildPath('README.md'), buildPath('dist', 'README.md'));
-    fs.copyFileSync(buildPath('LICENSE'), buildPath('dist', 'LICENSE'));
-
-    cli.action.stop();
+  protected preparePackageJson() {
+    const pkg = super.preparePackageJson();
+    pkg.main = 'index.cjs.js';
+    pkg.module = 'index.es.js';
+    return pkg;
   }
 }
