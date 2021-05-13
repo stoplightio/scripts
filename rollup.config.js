de1cb96376a4455b750ebc18558841597922b506
Jakub Rożek <jakub@stoplight.io>
2021-05-13 20:35:36 +0200

    feat(bundle): support package exports (#74)
    BREAKING CHANGE: support for package exports might cause unintended side effects, so to e safe we're marking this as a major. but hopefully it does not cause issues
    
    * feat(bundle): support package exports
    * chore: make js a commonjs by default
    * chore: emit TS declarations

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
-const typescript = require('@rollup/plugin-typescript');
+'use strict';
+const typescript = require('rollup-plugin-typescript2');
 const { terser } = require('rollup-plugin-terser');
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
 const path = require('path');
 const fs = require('fs');
 
 const BASE_PATH = process.cwd();
-const getConfigFile = (name) => {
+
+const getConfigFile = name => {
   const filePath = path.resolve(BASE_PATH, name);
   if (fs.existsSync(path.resolve(BASE_PATH, name))) {
     return filePath;
@@ -13,23 +17,32 @@ const getConfigFile = (name) => {
   return path.resolve(BASE_PATH, 'node_modules', '@stoplight', 'scripts', name);
 };
 
-module.exports = {
-  input: path.resolve(BASE_PATH, 'src/index.ts'),
-  plugins: [
+const plugins = () =>
+  [
     typescript({
       tsconfig: getConfigFile('tsconfig.build.json'),
-      include: ['src/**/*.{ts,tsx}'],
+      useTsconfigDeclarationDir: true,
     }),
-    terser(),
-  ],
-  output: [
-    {
-      file: path.resolve(BASE_PATH, 'dist/index.cjs.js'),
-      format: 'cjs'
+    process.env.MINIFY ? terser() : null,
+  ].filter(Boolean);
+
+module.exports = [
+  {
+    input: path.resolve(BASE_PATH, 'src/index.ts'),
+    output: {
+      entryFileNames: '[name].js',
+      dir: path.resolve(BASE_PATH, 'dist'),
+      format: 'cjs',
     },
-    {
-      file: path.resolve(BASE_PATH, 'dist/index.es.js'),
-      format: 'esm'
+    plugins: [json(), commonjs(), ...plugins()],
+  },
+  {
+    input: path.resolve(BASE_PATH, 'src/index.ts'),
+    output: {
+      entryFileNames: '[name].mjs',
+      dir: path.resolve(BASE_PATH, 'dist'),
+      format: 'esm',
     },
-  ],
-};
+    plugins: plugins(),
+  },
+];
