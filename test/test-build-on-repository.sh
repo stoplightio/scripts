745103109fc4419af3df9fabcdc3d33aee6a4930
Marc MacLeod <marc@stoplight.io>
2021-11-23 21:07:13 -0500

    try this...

--- test/test-build-on-repository.sh
+++ test/test-build-on-repository.sh
@@ -2,15 +2,14 @@ Repository=$1
 
 echo Testing repository $Repository
 
-mkdir temp
-cd temp
+cd ../
 
 git clone git@github.com:stoplightio/$Repository.git
 
 cd $Repository
 
 npx json -I -f package.json -e "this.resolutions={
-\"@stoplight/scripts\": \"file:../..\",
+\"@stoplight/scripts\": \"file:../project\",
 }"
 
 yarn
